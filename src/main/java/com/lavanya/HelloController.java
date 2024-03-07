58d4151d4515d7d25ee9a4914208d4af564b36be
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-03-07 14:19:54 -0500

    Added code to calculate  payment

--- src/main/java/com/lavanya/HelloController.java
+++ src/main/java/com/lavanya/HelloController.java
@@ -35,6 +35,12 @@ public class HelloController implements Initializable {
     public Label total;
     public TextField orderIdInput;
 
+
+    String selectedSize;
+    double  basePrice;
+    double tax=15;
+    double eachTopping=1.5;
+    double finalPrice=0;
     ObservableList<Modal> list= FXCollections.observableArrayList();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -61,5 +67,85 @@ public class HelloController implements Initializable {
     }
 
     public void continueBtn() {
+        setMsg("");
+        String name_=inputName.getText();
+        String toppings=inputToppings.getText();
+        String contact=inputContact.getText();
+
+        if(name_.equals("") || toppings.equals("") || contact.equals("")){
+            setMsg("NAME TOPPINGS CONTACT AND SIZE ARE REQUIRED");
+            return;
+        }
+
+        if(sizeS.isSelected()){
+            sizeL.setSelected(false);
+            sizeM.setSelected(false);
+            sizeXL.setSelected(false);
+            selectedSize=sizeS.getText();
+        }
+        else if (sizeL.isSelected()) {
+            sizeS.setSelected(false);
+            sizeM.setSelected(false);
+            sizeXL.setSelected(false);
+            selectedSize=sizeL.getText();
+
+        }else if (sizeM.isSelected()) {
+            sizeS.setSelected(false);
+            sizeL.setSelected(false);
+            sizeXL.setSelected(false);
+            selectedSize=sizeM.getText();
+
+        }else if (sizeXL.isSelected()) {
+            sizeL.setSelected(false);
+            sizeM.setSelected(false);
+            sizeXL.setSelected(false);
+            selectedSize=sizeXL.getText();
+
+
+        }
+        else{
+            setMsg("SIZE IS REQUIRED");
+            return;
+        }
+
+        billPane.setOpacity(1);
+
+        if(selectedSize.equals("s")){
+             basePrice=8.00;
+             double netPrice=basePrice+(Integer.parseInt(toppings)*eachTopping);
+             double afterTax=netPrice+((tax/100)*netPrice);
+             finalPrice=afterTax;
+
+            System.out.println(afterTax);
+
+        } else if (selectedSize.equals("m")) {
+            basePrice=10.00;
+            double netPrice=basePrice+(Integer.parseInt(toppings)*eachTopping);
+            double afterTax=netPrice+((tax/100)*netPrice);
+            finalPrice=afterTax;
+
+
+        } else if (selectedSize.equals("l")) {
+            basePrice=12.00;
+            double netPrice=basePrice+(Integer.parseInt(toppings)*eachTopping);
+            double afterTax=netPrice+((tax/100)*netPrice);
+            finalPrice=afterTax;
+
+
+        }
+        else {
+            basePrice=15.00;
+            double netPrice=basePrice+(Integer.parseInt(toppings)*eachTopping);
+            double afterTax=netPrice+((tax/100)*netPrice);
+            finalPrice=afterTax;
+
+        }
+
+        basicCost.setText(String.valueOf(basePrice));
+        toppingCost.setText(String.valueOf(Integer.parseInt(toppings)*eachTopping));
+        total.setText(String.valueOf(finalPrice));
+    }
+    public void setMsg(String msgg){
+        msg.setText(msgg);
     }
 }
