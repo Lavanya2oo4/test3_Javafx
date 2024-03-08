73acc4d0b930dcda0c8a6bb92b0bbd24bcbecb63
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-03-07 21:27:16 -0500

    Updated code

--- src/main/java/com/lavanya/HelloApplication.java
+++ src/main/java/com/lavanya/HelloApplication.java
@@ -18,7 +18,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-//        launch();
+        launch();
         System.out.println(calcBill(5,10));
     }
 
