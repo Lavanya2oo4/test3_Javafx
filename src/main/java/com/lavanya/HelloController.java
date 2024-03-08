ebe437ad2b95d5c4f0635856e5cce10e84066079
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-03-07 20:02:25 -0500

    Added code to place,view,delete orders

--- src/main/java/com/lavanya/HelloController.java
+++ src/main/java/com/lavanya/HelloController.java
@@ -9,8 +9,10 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 
+import java.net.ConnectException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.sql.*;
 
 public class HelloController implements Initializable {
 
@@ -37,11 +39,19 @@ public class HelloController implements Initializable {
 
 
     String selectedSize;
-    double  basePrice;
+    double  basePrice=0;
     double tax=15;
     double eachTopping=1.5;
+    double netToppingCost=0;
     double finalPrice=0;
     ObservableList<Modal> list= FXCollections.observableArrayList();
+
+    private final  String url="jdbc:mysql://localhost:3306/test3_pizza";
+    private final  String username="root";
+    private final  String password="Oreo@2004";
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         tableId.setCellValueFactory(new PropertyValueFactory<Modal,Integer>("orderId"));
@@ -55,12 +65,122 @@ public class HelloController implements Initializable {
 
 
     public void placeOrder() {
+        setMsg("");
+        tableview.getItems().clear();
+        String name_=inputName.getText();
+        String toppings=inputToppings.getText();
+        String contact=inputContact.getText();
+
+        if(name_.equals("") || toppings.equals("") || contact.equals("")){
+            setMsg("NAME TOPPINGS CONTACT AND SIZE ARE REQUIRED");
+
+            return;
+        }
+        try{
+            Integer.parseInt(toppings);
+        }
+        catch (Exception e){
+            setMsg("TOPPINGS MUST BE A NUMBER");
+            return;
+        }
+
+        try{
+//            Class.forName("com.cj.mysql.jdbc.Driver");
+
+        }
+        catch(Exception e){
+            System.out.println( e);
+        }
+        try{
+            Connection connection=DriverManager.getConnection(url,username,password);
+            String query="INSERT INTO orders (name,contact,size,toppings,total) VALUE (?,?,?,?,?) ";
+            PreparedStatement preparedStatement=connection.prepareStatement(query);
+            preparedStatement.setString(1,name_);
+            preparedStatement.setString(2,contact);
+            preparedStatement.setString(3,selectedSize);
+            preparedStatement.setInt(4,Integer.parseInt(toppings));
+            preparedStatement.setDouble(5,finalPrice);
+
+            int rowsAffected=preparedStatement.executeUpdate();
+            if(rowsAffected>0){
+                setMsg("ORDER PLACED SUCCESSFULLY");
+                fetchAll();
+            }
+            else{
+                setMsg("FAILED TO PLACE ORDER");
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public void deletOrder() {
+        String orderId=orderIdInput.getText();
+        if(orderId.equals("")){
+            setMsg("ORDER ID IS NEEDED");
+            return;
+        }
+
+        try{
+            Class.forName("com.cj.mysql.jdbc.Driver");
+
+        }
+        catch(Exception e){
+//            System.out.println( e);
+        }
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            String query = "DELETE FROM orders WHERE orderID=? ";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, Integer.parseInt(orderId));
+
+            int rowsAffected=preparedStatement.executeUpdate();
+            if(rowsAffected>0){
+                setMsg("ORDER DELETED SUCCESSFULLY");
+
+            }
+            else{
+                setMsg("FAILED TO DELETE ORDER");
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public void viewOrders() {
+        setMsg("");
+        fetchAll();
+    }
+    public void fetchAll(){
+        tableview.getItems().clear();
+        try{
+            Class.forName("com.cj.mysql.jdbc.Driver");
+
+        }
+        catch(Exception e){
+//            System.out.println( e);
+        }
+        try{
+            Connection connection=DriverManager.getConnection(url,username,password);
+            String query="SELECT * FROM orders ";
+            PreparedStatement preparedStatement=connection.prepareStatement(query);
+
+            ResultSet resultSet=preparedStatement.executeQuery();
+            while(resultSet.next()){
+                int orderId=resultSet.getInt("orderId");
+                String name=resultSet.getString("name");
+                String contact=resultSet.getString("contact");
+                String size=resultSet.getString("size");
+                int toppings=resultSet.getInt("toppings");
+                double total=resultSet.getDouble("total");
+                tableview.getItems().add(new Modal(orderId,name,contact,size,toppings,total));
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public void updateOrder() {
@@ -74,6 +194,13 @@ public class HelloController implements Initializable {
 
         if(name_.equals("") || toppings.equals("") || contact.equals("")){
             setMsg("NAME TOPPINGS CONTACT AND SIZE ARE REQUIRED");
+            return;
+        }
+        try{
+            Integer.parseInt(toppings);
+        }
+        catch (Exception e){
+            setMsg("TOPPINGS MUST BE A NUMBER");
             return;
         }
 
@@ -112,22 +239,23 @@ public class HelloController implements Initializable {
 
         if(selectedSize.equals("s")){
              basePrice=8.00;
-             double netPrice=basePrice+(Integer.parseInt(toppings)*eachTopping);
-             double afterTax=netPrice+((tax/100)*netPrice);
-             finalPrice=afterTax;
-
-            System.out.println(afterTax);
+            netToppingCost=(Integer.parseInt(toppings)*eachTopping);
+            double netPrice=basePrice+netToppingCost;
+            double afterTax=netPrice+((tax/100)*netPrice);
+            finalPrice=afterTax;
 
         } else if (selectedSize.equals("m")) {
             basePrice=10.00;
-            double netPrice=basePrice+(Integer.parseInt(toppings)*eachTopping);
+            netToppingCost=(Integer.parseInt(toppings)*eachTopping);
+            double netPrice=basePrice+netToppingCost;
             double afterTax=netPrice+((tax/100)*netPrice);
             finalPrice=afterTax;
 
 
         } else if (selectedSize.equals("l")) {
             basePrice=12.00;
-            double netPrice=basePrice+(Integer.parseInt(toppings)*eachTopping);
+            netToppingCost=(Integer.parseInt(toppings)*eachTopping);
+            double netPrice=basePrice+netToppingCost;
             double afterTax=netPrice+((tax/100)*netPrice);
             finalPrice=afterTax;
 
@@ -135,14 +263,16 @@ public class HelloController implements Initializable {
         }
         else {
             basePrice=15.00;
-            double netPrice=basePrice+(Integer.parseInt(toppings)*eachTopping);
+            netToppingCost=(Integer.parseInt(toppings)*eachTopping);
+            double netPrice=basePrice+netToppingCost;
             double afterTax=netPrice+((tax/100)*netPrice);
             finalPrice=afterTax;
+
 
         }
 
         basicCost.setText(String.valueOf(basePrice));
-        toppingCost.setText(String.valueOf(Integer.parseInt(toppings)*eachTopping));
+        toppingCost.setText(String.valueOf(netToppingCost));
         total.setText(String.valueOf(finalPrice));
     }
     public void setMsg(String msgg){
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.lavanya {
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens com.lavanya to javafx.fxml;
     exports com.lavanya;
